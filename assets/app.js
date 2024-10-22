9a305cb05d4dd8ac54cd502d9acd743ba98b68f0
linusmaria <linus.hausammann@sbw-media.ch>
2024-10-22 15:49:41 +0200

    hilfe

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,36 @@
 "use strict"
 // opendata api URL
-const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
-
-const getData = async () => {};
-
-const displayData = (data) => {};
+ 
+const stationboard = {
+    data() {
+        return {
+            station: "",
+            stationName: "",
+            trains: []
+        }
+    },
+    methods: {
+        async loadConnections() {
+            if (this.station === "") return;
+            const url = `https://transport.opendata.ch/v1/stationboard?station=${this.station}`;
+            const response = await fetch(url);
+            const data = await response.json();
+ 
+            this.stationName = data.station.name;
+            this.trains = data.stationboard;
+ 
+ 
+        },
+        timestampToTime(timestamp) {
+            const date = new Date(timestamp);
+            return date.toLocaleTimeString("de-CH");
+         }
+    }
+};
+ 
+const app = Vue.createApp(stationboard);
+ 
+app.mount('#app');
+ 
+ 
+ 
